4093076a71e7a894ed2041864d67d621f7f3eb06
Kalvin Nguyen <kalvin.win59@gmail.com>
2021-11-24 11:14:48 -0600

    heroku deploy

--- server.js
+++ server.js
@@ -13,10 +13,13 @@ app.use(express.json());
 
 app.use(express.static('public'));
 
-mongoose.connect(process.env.MONGODB_URI ||'mongodb://localhost/workout', {
+mongoose.connect
+    (process.env.MONGODB_URI ||'mongodb://localhost/workout', 
+    {
     useNewUrlParser: true,
     useFindAndModify: false,
     useUnifiedTopology: true,
+    useCreateIndex: true,
 });
 
 app.use(require('./routes/api.js'));
